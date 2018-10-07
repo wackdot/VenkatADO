bc5ca474d6d4a5266a7bab6b0e61eec1363a5716
Gab <wackdot2002@gmail.com>
2018-10-07 22:38:05 +0800

    Update!

---
+++ AdoDemo/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace AdoDemo
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Sample;Integrated Security=True; MultipleActiveResultSets=True");
+			SqlCommand cmd = new SqlCommand("SELECT * FROM Product", con);
+			con.Open();
+			SqlDataReader rdr = cmd.ExecuteReader();
+
+			GridView1.DataSource = rdr;
+			GridView1.DataBind();
+
+			con.Close();
+		}
+
+	}
+}
