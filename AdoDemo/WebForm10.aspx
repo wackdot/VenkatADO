cc428eafddb19696c14ec7c5993defe0d20b27b5
Gab <wackdot2002@gmail.com>
2018-10-12 22:15:43 +0800

    10:15 PM 12/10/18 Update!

---
+++ AdoDemo/WebForm10.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm10.aspx.cs" Inherits="AdoDemo.WebForm10" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Btn_GetData" runat="server" Text="Get Data from Database" OnClick="Btn_GetData_Click" />
+            <br />
+            <br />
+            <asp:GridView ID="Gvw_Students" runat="server" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4" AutoGenerateColumns="False" DataKeyNames="Id" OnRowCancelingEdit="Gvw_Students_RowCancelingEdit" OnRowDeleting="Gvw_Students_RowDeleting" OnRowEditing="Gvw_Students_RowEditing" OnRowUpdating="Gvw_Students_RowUpdating">
+                <Columns>
+                    <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" />
+                    <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True" SortExpression="Id" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                    <asp:BoundField DataField="TotalMarks" HeaderText="TotalMarks" SortExpression="TotalMarks" />
+                </Columns>
+                <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
+                <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
+                <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
+                <RowStyle BackColor="White" ForeColor="#330099" />
+                <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
+                <SortedAscendingCellStyle BackColor="#FEFCEB" />
+                <SortedAscendingHeaderStyle BackColor="#AF0101" />
+                <SortedDescendingCellStyle BackColor="#F6F0C0" />
+                <SortedDescendingHeaderStyle BackColor="#7E0000" />
+            </asp:GridView>
+            <br />
+            <asp:Button ID="Btn_UpdateDatabase" runat="server" Text="Update Database Table" OnClick="Btn_UpdateDatabase_Click" />
+&nbsp;
+            <asp:Label ID="Lbl_Message" runat="server"></asp:Label>
+            <br />
+            <br />
+<%--            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:DBCS %>" DeleteCommand="DELETE FROM [Students] WHERE [Id] = @Id" InsertCommand="INSERT INTO [Students] ([Name], [Gender], [TotalMarks]) VALUES (@Name, @Gender, @TotalMarks)" SelectCommand="SELECT * FROM [Students]" UpdateCommand="UPDATE [Students] SET [Name] = @Name, [Gender] = @Gender, [TotalMarks] = @TotalMarks WHERE [Id] = @Id">
+                <DeleteParameters>
+                    <asp:Parameter Name="Id" Type="Int32" />
+                </DeleteParameters>
+                <InsertParameters>
+                    <asp:Parameter Name="Name" Type="String" />
+                    <asp:Parameter Name="Gender" Type="String" />
+                    <asp:Parameter Name="TotalMarks" Type="Int32" />
+                </InsertParameters>
+                <UpdateParameters>
+                    <asp:Parameter Name="Name" Type="String" />
+                    <asp:Parameter Name="Gender" Type="String" />
+                    <asp:Parameter Name="TotalMarks" Type="Int32" />
+                    <asp:Parameter Name="Id" Type="Int32" />
+                </UpdateParameters>
+            </asp:SqlDataSource>--%>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdoDemo/WebForm10.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdoDemo
+{
+	public partial class WebForm10 : System.Web.UI.Page
+	{
+
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void GetDataFromDb()
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+			SqlConnection con = new SqlConnection(connectionString);
+			string strSelectQuery = "SELECT * FROM Students";
+			SqlDataAdapter da = new SqlDataAdapter(strSelectQuery, connectionString);
+
+			DataSet ds = new DataSet();
+			da.Fill(ds, "Students");
+
+			ds.Tables["Students"].PrimaryKey = new DataColumn[] { ds.Tables["Students"].Columns["Id"] };
+			Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+
+
+			Gvw_Students.DataSource = ds;
+			Gvw_Students.DataBind();
+
+			Lbl_Message.Text = "Data Loaded from Database";
+		}
+
+		private void GetDataFromCache()
+		{
+			if (Cache["DATASET"] != null)
+			{
+				DataSet ds = (DataSet)Cache["DATASET"];
+				Gvw_Students.DataSource = ds;
+				Gvw_Students.DataBind();
+			}
+		}
+
+		protected void Btn_GetData_Click(object sender, EventArgs e)
+		{
+			GetDataFromDb();
+		}
+
+		protected void Gvw_Students_RowEditing(object sender, GridViewEditEventArgs e)
+		{
+			Gvw_Students.EditIndex = e.NewEditIndex;
+			GetDataFromCache();
+		}
+
+		protected void Gvw_Students_RowUpdating(object sender, GridViewUpdateEventArgs e)
+		{
+			if (Cache["DATASET"] != null)
+			{
+				DataSet ds = (DataSet)Cache["DATASET"];
+				DataRow dr = ds.Tables["Students"].Rows.Find(e.Keys["Id"]);
+
+				dr["Name"] = e.NewValues["Name"];
+				dr["Gender"] = e.NewValues["Gender"];
+				dr["TotalMarks"] = e.NewValues["TotalMarks"];
+
+				Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+				Gvw_Students.EditIndex = -1;
+				GetDataFromCache();
+			}
+		}
+
+		protected void Gvw_Students_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+		{
+			Gvw_Students.EditIndex = -1;
+			GetDataFromCache();
+		}
+
+		protected void Gvw_Students_RowDeleting(object sender, GridViewDeleteEventArgs e)
+		{
+			if (Cache["DATASET"] != null)
+			{
+				DataSet ds = (DataSet)Cache["DATASET"];
+				DataRow dr = ds.Tables["Students"].Rows.Find(e.Keys["Id"]);
+
+				dr.Delete();
+
+				Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+				GetDataFromCache();
+			}
+		}
+
+		protected void Btn_UpdateDatabase_Click(object sender, EventArgs e)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+			SqlConnection con = new SqlConnection(connectionString);
+			string strSelectQuery = "SELECT * FROM Students";
+			SqlDataAdapter da = new SqlDataAdapter(strSelectQuery, connectionString);
+
+			DataSet ds = (DataSet)Cache["DATASET"];
+
+			string strUpdateCommand = "UPDATE Students SET Name = @Name, Gender = @Gender, TotalMarks = @TotalMarks WHERE Id = @Id";
+			SqlCommand updateCommand = new SqlCommand(strUpdateCommand, con);
+			updateCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+			updateCommand.Parameters.Add("@Name", SqlDbType.VarChar, 255, "Name");
+			updateCommand.Parameters.Add("@Gender", SqlDbType.VarChar, 255, "Gender");
+			updateCommand.Parameters.Add("@TotalMarks", SqlDbType.Int, 0, "TotalMarks");
+
+			da.UpdateCommand = updateCommand;
+
+			string strDeleteCommand = "DELETE FROM Students WHERE Id = @Id";
+			SqlCommand deleteCommand = new SqlCommand(strDeleteCommand, con);
+			deleteCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+
+			da.DeleteCommand = deleteCommand;
+
+			da.Update(ds, "Students");
+
+			Lbl_Message.Text = "Database Table Updated";
+		}
+	}
+}
