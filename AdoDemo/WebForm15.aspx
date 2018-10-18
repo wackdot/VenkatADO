2162ffe0c6910763e9913f866b31d049d5ef0b0a
wackdot <wackdot2002@gmail.com>
2018-10-18 17:28:37 +0800

    Update

---
+++ AdoDemo/WebForm15.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm15.aspx.cs" Inherits="AdoDemo.WebForm15" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Btn_BulkTransfer" runat="server" OnClick="Btn_BulkTransfer_Click" Text="Start Bulk Transfer" />
+            <br />
+            <br />
+            <asp:Label ID="Lbl_ProgressStatus" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdoDemo/WebForm15.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdoDemo
+{
+	public partial class WebForm15 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+	
+		}
+
+		private void bc_SqlRowsCopied(object sender, SqlRowsCopiedEventArgs e)
+		{
+			Lbl_ProgressStatus.Text = Lbl_ProgressStatus.Text + "<br/>" + e.RowsCopied + " loaded...";
+		}
+
+		protected void Btn_BulkTransfer_Click(object sender, EventArgs e)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+			using (SqlConnection sourceCon = new SqlConnection(connectionString))
+			{
+				SqlCommand cmd = new SqlCommand("SELECT * FROM Products_Source", sourceCon);
+				sourceCon.Open();
+
+				using (SqlDataReader reader = cmd.ExecuteReader())
+				{
+					using (SqlConnection destinationCon = new SqlConnection(connectionString))
+					{
+						using (SqlBulkCopy bc = new SqlBulkCopy(destinationCon))
+						{
+							bc.BatchSize = 10000;
+							bc.NotifyAfter = 5000;
+							bc.SqlRowsCopied += new SqlRowsCopiedEventHandler(bc_SqlRowsCopied);
+							bc.DestinationTableName = "Products_Destination";
+							destinationCon.Open();
+							bc.WriteToServer(reader);
+						}
+					}
+				}
+
+			}
+		}
+	}
+}
