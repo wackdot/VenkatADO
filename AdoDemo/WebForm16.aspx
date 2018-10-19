a1414f0d3273afc4b6bc9211d30f2107da339849
wackdot <wackdot2002@gmail.com>
2018-10-19 14:10:34 +0800

    Update!

---
+++ AdoDemo/WebForm16.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm16.aspx.cs" Inherits="AdoDemo.WebForm16" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="text-align: left; color: white; background-color: darkred">
+                <tr>
+                    <th>Account Number</th>
+                    <td><asp:Label ID="Lbl_AccountNo1" runat="server"></asp:Label></td>
+                    <td><asp:Label ID="Lbl_AccountNo2" runat="server"></asp:Label></td>
+                </tr>
+                <tr>
+                    <th>Customer Name</th>
+                    <td><asp:Label ID="Lbl_CustName1" runat="server"></asp:Label></td>
+                    <td><asp:Label ID="Lbl_CustName2" runat="server"></asp:Label></td>
+                </tr>
+                <tr>
+                    <th>Balance</th>
+                    <td><asp:Label ID="Lbl_Balance1" runat="server"></asp:Label></td>
+                    <td><asp:Label ID="Lbl_Balance2" runat="server"></asp:Label></td>
+                </tr>
+            </table>
+            <br />
+            <asp:Button ID="Btn_Transfer" runat="server" Text="Transfer $10 from A1 to A2" OnClick="Btn_Transfer_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Lbl_Message" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdoDemo/WebForm16.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdoDemo
+{
+	public partial class WebForm16 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				GetData();
+			}
+		}
+
+		protected void Btn_Transfer_Click(object sender, EventArgs e)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+			using (SqlConnection con = new SqlConnection(connectionString))
+			{
+				con.Open();
+				SqlTransaction transaction = con.BeginTransaction();
+
+				try
+				{				
+					SqlCommand cmd = new SqlCommand("UPDATE Accounts SET Balance = Balance - 10 WHERE AccountNumber = 'A1'", con, transaction);										
+					cmd.ExecuteNonQuery();
+
+					cmd = new SqlCommand("UPDATE Accounts SET Balance = Balance + 10 WHERE AccountNumber = 'A2'", con, transaction);
+					cmd.ExecuteNonQuery();
+
+					transaction.Commit();
+
+					Lbl_Message.Text = "Transaction Successful!";
+					Lbl_Message.ForeColor = System.Drawing.Color.Green;					
+				}
+				catch
+				{
+					transaction.Rollback();
+					Lbl_Message.Text = "Transaction Failed!";
+					Lbl_Message.ForeColor = System.Drawing.Color.Red;
+				}
+				finally
+				{
+					GetData();
+				}
+			}
+		}
+
+		private void GetData()
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+			using (SqlConnection con = new SqlConnection(connectionString))
+			{
+				SqlCommand cmd = new SqlCommand("SELECT * FROM Accounts", con);
+				con.Open();
+
+				SqlDataReader reader = cmd.ExecuteReader();
+
+				while (reader.Read())
+				{
+					if (reader["AccountNumber"].ToString() == "A1")
+					{
+						Lbl_AccountNo1.Text = reader["AccountNumber"].ToString();
+						Lbl_CustName1.Text = reader["CustomerName"].ToString();
+						Lbl_Balance1.Text = reader["Balance"].ToString();
+					}
+					else if (reader["AccountNumber"].ToString() == "A2")
+					{
+						Lbl_AccountNo2.Text = reader["AccountNumber"].ToString();
+						Lbl_CustName2.Text = reader["CustomerName"].ToString();
+						Lbl_Balance2.Text = reader["Balance"].ToString();						
+					}
+				}
+			}
+		}
+
+	}
+}
